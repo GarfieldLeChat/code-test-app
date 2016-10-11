95de1bc814035606797fccd3c4bbbdc5587f09a4
GarfieldLeChat <admin@lucidlondon.co.uk>
2016-10-11 19:22:57 +0100

    Basic Search app built
    foursquare code test app built.
    
    TODO: Incorporate GULP, Unit Tests

---
+++ style/gulp.sh
@@ -0,0 +1,8 @@
+npm install
+npm install gulp --save-dev
+npm install gulp-concat --save-dev
+npm install gulp-connect --save-dev
+npm install gulp-rename --save-dev
+npm install gulp-sass --save-dev
+npm install gulp-util --save-dev
+npm install --save-dev gulp.spritesmith
---
+++ style/gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    connect = require('gulp-connect'),
+    rename = require('gulp-rename'),
+	uglify = require('gulp-uglify'),
+	gulpif = require('gulp-if'),
+    args   = require('yargs').argv;
+
+var jsSources = ['js/main.js'],
+    jsDirectory = ['js/**/*.js'],
+	jsDirectoryMods = ['js/modules/*.js'],
+    sassSources = ['sass/*.scss'],
+    cssOutputDir = 'css/',
+    outputDir = 'js/global.js';
+
+gulp.task('sass', function() {
+  gulp.src(sassSources)
+  .pipe(sass({global: 'compressed', outputStyle: 'compressed'}))
+    .on('error', gutil.log)
+  .pipe(gulp.dest('css/'))
+  .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+  gulp.src(jsDirectoryMods)
+  .pipe(concat('global.js'))
+  .pipe(gulp.dest('js'))
+  .pipe(rename('global-min.js'))
+  .pipe(gulpif(args.production, uglify().on('error', gutil.log)))
+  .pipe(gulp.dest('js'))
+  .pipe(connect.reload());
+});
+
+gulp.task('reload', function(){
+  gulp.src(jsDirectory)
+  .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch(jsDirectoryMods, ['js']);
+  gulp.watch(jsDirectory, ['reload']);
+  gulp.watch(sassSources, ['sass']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '.',
+    livereload: true
+  });
+});
+
+gulp.task('build', function() {
+    gulp.start('sass', 'js');
+});
+
+gulp.task('default', ['sass', 'js', 'connect', 'watch']);
