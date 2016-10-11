726c7076f6fbc3cc0e8f7ce91efd8324a31dec69
GarfieldLeChat <admin@lucidlondon.co.uk>
2016-10-11 22:00:46 +0100

    Unit test titles added

---
+++ spec/controllerSpec.js
@@ -0,0 +1,15 @@
+// General Api and connection tests
+
+// Unit test for connection to both apis
+
+// Search based tests
+
+// Error handling and exceptions
+
+// No results result check
+
+// Unit test to check filter is cached locally
+
+// Unit test to check filter is defaulting if no options selected
+
+// Unit test to validate returned JSON has all the required fields
